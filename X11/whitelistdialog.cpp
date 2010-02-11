2633d3f6c22be16452caf997f31f0e5a4f6cb73b
kalev <kalev@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2010-02-11 12:53:21 +0000

    plugins/browser: Use custom data model for storing whitelist in gtkmm UI
    git-svn-id: https://esteid.googlecode.com/svn/plugins/browser/trunk@2570 0d7e1ef0-d974-fc83-f0b3-ccc071561e0e

--- X11/whitelistdialog.cpp
+++ X11/whitelistdialog.cpp
@@ -12,34 +12,57 @@ WhitelistDialog::WhitelistDialog(BaseObjectType* cobject, const Glib::RefPtr<Gtk
                     &WhitelistDialog::on_button_cancel) );
     }
 
-    m_refGlade->get_widget("scrolledWindow", m_scrolledWindow);
-
-    m_whitelistView = new Gtk::ListViewText(1);
-    m_scrolledWindow->add(*m_whitelistView);
-    m_whitelistView->set_headers_visible(false);
+    m_whitelistView = getTreeView();
 
 
+    // Insert dummy sites
     std::vector<std::string> sv;
     sv.push_back("id.swedbank.ee");
     sv.push_back("id.seb.ee");
     sv.push_back("id.eesti.ee");
 
-    m_whitelistView->clear_items();
-
-    guint row_number;
-
-    std::vector<std::string>::const_iterator it;
-    for (it = sv.begin(); it != sv.end(); ++it) {
-        row_number = m_whitelistView->append_text();
-        m_whitelistView->set_text(row_number,  0, (*it).c_str());
-    }
-
-    m_whitelistView->show();
+    m_listModel->clear();
+    addSites(sv);
 }
+
 
 WhitelistDialog::~WhitelistDialog()
 {
 }
+
+
+Gtk::TreeView *WhitelistDialog::getTreeView()
+{
+    Gtk::TreeView *treeview;
+
+    m_refGlade->get_widget("whitelistView", treeview);
+
+    m_listModel = Gtk::ListStore::create(m_listColumns);
+    m_listModel->set_sort_column_id(0, Gtk::SORT_ASCENDING);
+    treeview->set_model(m_listModel);
+    treeview->append_column("Sites", m_listColumns.site);
+
+    return treeview;
+}
+
+
+void WhitelistDialog::addSites(const std::vector<std::string> & sv)
+{
+    std::vector<std::string>::const_iterator it;
+
+    for (it = sv.begin(); it != sv.end(); ++it)
+        addSite(*it);
+}
+
+
+void WhitelistDialog::addSite(const std::string & site)
+{
+    Gtk::TreeModel::iterator it;
+
+    it = m_listModel->append();
+    (*it)[m_listColumns.site] = site;
+}
+
 
 void WhitelistDialog::on_button_cancel()
 {
--- X11/whitelistdialog.h
+++ X11/whitelistdialog.h
@@ -11,14 +11,29 @@ public:
     virtual ~WhitelistDialog();
 
 protected:
+    class WhitelistModelColumns : public Gtk::TreeModel::ColumnRecord
+    {
+    public:
+        Gtk::TreeModelColumn<Glib::ustring> site;
+
+        WhitelistModelColumns() { add(site); }
+    };
+
+
+    Gtk::TreeView *getTreeView();
+    void addSites(const std::vector<std::string> & sv);
+    void addSite(const std::string & site);
+
     // Signal handlers:
     void on_button_cancel();
 
     Glib::RefPtr<Gtk::Builder> m_refGlade;
 
-    Gtk::ScrolledWindow *m_scrolledWindow;
-    Gtk::ListViewText *m_whitelistView;
     Gtk::Button *m_cancelButton;
+
+    Gtk::TreeView *m_whitelistView;
+    WhitelistModelColumns m_listColumns;
+    Glib::RefPtr<Gtk::ListStore> m_listModel;
 };
 
 #endif //ESTEID_WHITELISTDIALOG_H
