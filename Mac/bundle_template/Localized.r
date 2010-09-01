2c042968bcccc7388defb05f04ee68ec159bbf0c
kalevlember <kalevlember@0d7e1ef0-d974-fc83-f0b3-ccc071561e0e>
2010-09-01 21:23:10 +0000

    esteid-browser-plugin: Register application/x-idcard-plugin on Mac
    git-svn-id: https://esteid.googlecode.com/svn/esteid-browser-plugin/trunk@2979 0d7e1ef0-d974-fc83-f0b3-ccc071561e0e

--- Mac/bundle_template/Localized.r
+++ Mac/bundle_template/Localized.r
@@ -8,11 +8,14 @@ resource 'STR#' (126)
 
 resource 'STR#' (127) 
 { {
+    "${FBSTRING_FileDescription}",
     "${FBSTRING_FileDescription}"
 } };
 
 resource 'STR#' (128) 
 { {
     "${FBSTRING_MIMEType}",
+    "${FBSTRING_FileExtents}",
+    "application/x-idcard-plugin",
     "${FBSTRING_FileExtents}"
 } };
